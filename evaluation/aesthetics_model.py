b6f99f8776df9903d4fc8f6c014750cf579261f6
Luca Andrea Sbicego <lsbicego@izar1.izar.cluster>
2025-05-05 12:14:41 +0200

    finalise eval functionalities

--- evaluation/aesthetics_model.py
+++ evaluation/aesthetics_model.py
@@ -1,13 +1,11 @@
-### Unclean code courtesy of
+### Unclean code courtesy of https://github.com/christophschuhmann/improved-aesthetic-predictor
 
 
 import numpy as np
 import torch
-import pytorch_lightning as pl
 import torch.nn as nn
 import torch.nn.functional as F
-
-import clip
+from transformers import CLIPProcessor, CLIPModel
 
 
 def normalized(a, axis=-1, order=2):
@@ -18,18 +16,25 @@ def normalized(a, axis=-1, order=2):
 def get_aesthetic_model(device):
     model = MLP(768)  # CLIP embedding dim is 768 for CLIP ViT L 14
 
-    s = torch.load("sac+logos+ava1-l14-linearMSE.pth")   # load the model you trained previously or the model available in this repo
+    s = torch.load("sac+logos+ava1-l14-linearMSE.pth", weights_only=False)   # load the model you trained previously or the model available in this repo
     model.load_state_dict(s)
 
     model.to(device)
     model.eval()
 
     # also load clip model
-    clip_model, preprocess = clip.load("ViT-L/14", device=device)  #RN50x64   
+    # clip_model, preprocess = clip.load("ViT-L/14", device=device)  #RN50x64 
+
+    clip_model = CLIPModel.from_pretrained("openai/clip-vit-large-patch14")
+    clip_model = clip_model.to(device)
+
+    # processor.image_processor is a torchvision.Compose of:
+    #   Resize(224, BICUBIC) → CenterCrop(224) → ToTensor() → Normalize(mean, std)
+    preprocess = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")
 
     return model, clip_model, preprocess
 
-class MLP(pl.LightningModule):
+class MLP(nn.Module):
     def __init__(self, input_size, xcol='emb', ycol='avg_rating'):
         super().__init__()
         self.input_size = input_size
